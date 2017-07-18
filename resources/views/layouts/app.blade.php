208d12b9902064f4c687424ff2a3b4ed48db6de5
41Studio <fauziiirachman@gmail.com>
2017-07-18 16:45:38 +0700

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,66 @@
 
   <body>
 
+    <style>
+        .img-footer{
+        width: 110px;
+        height: 94px;
+        }
+        footer.nb-footer {
+        background: #222;
+        border-top: 4px solid #b78c33; }
+        footer.nb-footer .about {
+        margin: 0 auto;
+        margin-top: 40px;
+        max-width: 1170px;
+        text-align: center; }
+        footer.nb-footer .about p {
+        font-size: 13px;
+        color: #999;
+        margin-top: 30px; }
+        footer.nb-footer .about .social-media {
+        margin-top: 15px; }
+        footer.nb-footer .about .social-media ul li a {
+        display: inline-block;
+        width: 45px;
+        height: 45px;
+        line-height: 45px;
+        border-radius: 50%;
+        font-size: 16px;
+        color: #b78c33;
+        border: 1px solid rgba(255, 255, 255, 0.3); }
+        footer.nb-footer .about .social-media ul li a:hover {
+        background: #b78c33;
+        color: #fff;
+        border-color: #b78c33; }
+        footer.nb-footer .footer-info-single {
+        margin-top: 30px; }
+        footer.nb-footer .footer-info-single .title {
+        color: #aaa;
+        text-transform: uppercase;
+        font-size: 16px;
+        border-left: 4px solid #b78c33;
+        padding-left: 5px; }
+        footer.nb-footer .footer-info-single ul li a {
+        display: block;
+        color: #aaa;
+        padding: 2px 0; }
+        footer.nb-footer .footer-info-single ul li a:hover {
+        color: #b78c33; }
+        footer.nb-footer .footer-info-single p {
+        font-size: 13px;
+        line-height: 20px;
+        color: #aaa; }
+        footer.nb-footer .copyright {
+        margin-top: 15px;
+        background: #111;
+        padding: 7px 0;
+        color: #999; }
+        footer.nb-footer .copyright p {
+        margin: 0;
+        padding: 0; }
+    </style>
+
     <div class="container-narrow">
 
       <div class="masthead">
@@ -58,11 +118,91 @@
 
       @yield('content')
 
-      <div class="footer">
-        <p>&copy; NiagaArt 2017</p>
-      </div>
+    </div>
 
-    </div> <!-- /container -->
+      <footer class="nb-footer">
+        <div class="container">
+        <div class="row">
+        <div class="col-sm-12">
+        <div class="about">
+        	<img src="img/logo-footer.png" class="img-responsive center-block img-footer" alt="">
+        </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+        <div class="footer-info-single">
+        	<h2 class="title">Interior Design</h2>
+        	<ul class="list-unstyled">
+        		<li><a href="{{ route('service', ['name' => 'home-living']) }}" title=""><i class="fa fa-angle-double-right"></i> Home Living</a></li>
+        		<li><a href="{{ route('service', ['name' => 'apartment']) }}" title=""><i class="fa fa-angle-double-right"></i> Apartment</a></li>
+        		<li><a href="{{ route('service', ['name' => 'cafe-foodcourt']) }}" title=""><i class="fa fa-angle-double-right"></i> Cafe & FoodCourt</a></li>
+        		<li><a href="{{ route('service', ['name' => 'shop-office']) }}" title=""><i class="fa fa-angle-double-right"></i> Shop / Office</a></li>
+        	</ul>
+        </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+        <div class="footer-info-single">
+        	<h2 class="title">Other Interior</h2>
+        	<ul class="list-unstyled">
+        		<li><a href="{{ route('service', ['name' => 'flooring']) }}" title=""><i class="fa fa-angle-double-right"></i> Flooring</a></li>
+        		<li><a href="{{ route('service', ['name' => 'blind']) }}" title=""><i class="fa fa-angle-double-right"></i> Blind</a></li>
+        		<li><a href="{{ route('service', ['name' => 'wall-paper']) }}" title=""><i class="fa fa-angle-double-right"></i> Wall paper</a></li>
+        		<li><a href="{{ route('service', ['name' => 'wall-clading']) }}" title=""><i class="fa fa-angle-double-right"></i> Wall Clading</a></li>
+        		<li><a href="{{ route('service', ['name' => 'chair']) }}" title=""><i class="fa fa-angle-double-right"></i> Chair</a></li>
+        	</ul>
+        </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+        <div class="footer-info-single">
+        	<h2 class="title">Products</h2>
+        	<ul class="list-unstyled">
+        		<li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Table</a></li>
+        		<li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Chair</a></li>
+        		<li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Wardrobe</a></li>
+        	</ul>
+        </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+        <div class="footer-info-single">
+        	<h2 class="title">Contact</h2>
+        	<p>Office Address at Jl. Sangkuriang no.43 Cimahi Utara, West Java, Indonesia (022) 878041654</p>
+
+        </div>
+        </div>
+        </div>
+        </div>
+
+        <div class="col-sm-12">
+        <div class="about">
+
+        	<div class="social-media">
+        		<ul class="list-inline">
+        			<li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+        			<li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
+        			<li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
+        			<li><a href="#" title=""><i class="fa fa-linkedin"></i></a></li>
+        			<li><a href="#" title=""><i class="fa fa-linkedin"></i></a></li>
+        		</ul>
+        	</div>
+        </div>
+        </div>
+
+        <section class="copyright">
+        <div class="container">
+        <div class="row">
+        <div class="col-sm-6">
+        <p>Copyright © 2017. NiagaArt.</p>
+        </div>
+        <div class="col-sm-6"></div>
+        </div>
+        </div>
+        </section>
+      </footer>
+
+     <!-- /container -->
 
     <!-- Le javascript
     ================================================== -->
