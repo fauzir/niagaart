4ae92f99fdaeb7776ce10ff187fc622c30dea327
41Studio <fauziiirachman@gmail.com>
2017-08-01 10:40:54 +0700

    clear function on the route

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App;
 use DB;
 use App\Blog;
 use App\BlogTag;
@@ -93,5 +94,50 @@ class BlogController extends Controller
         $socials = Social::where('active', 'yes')->get();
         $contact = Contact::find(1);
         return view('blog-category', compact('servicefooters', 'blogs', 'categories', 'count', 'populars', 'rel', 'socials', 'contact'));
+    }
+
+    public function getFeed()
+    {
+        // create new feed
+        $feed = App::make("feed");
+
+        // multiple feeds are supported
+        // if you are using caching you should set different cache keys for your feeds
+
+        // cache the feed for 60 minutes (second parameter is optional)
+        $feed->setCache(60, 'laravelFeedKey');
+
+        // check if there is cached feed and build new only if is not
+        if (!$feed->isCached())
+        {
+           // creating rss feed with our most recent 20 posts
+           $posts = DB::table('blogs')->orderBy('created_at', 'desc')->take(20)->get();
+
+           // set your feed's title, description, link, pubdate and language
+           $feed->title = 'Niaga Art Blog';
+           $feed->description = 'Blog on interior design, other interior, and projects';
+           $feed->logo = 'http://yoursite.tld/logo.jpg';
+           $feed->link = url('feed');
+           $feed->setDateFormat('datetime'); // 'datetime', 'timestamp' or 'carbon'
+           $feed->pubdate = $posts[0]->created_at;
+           $feed->lang = 'en';
+           $feed->setShortening(true); // true or false
+           $feed->setTextLimit(100); // maximum length of description text
+
+           foreach ($posts as $post)
+           {
+               // set item's title, author, url, pubdate, description, content, enclosure (optional)*
+               $feed->add($post->title, $post->author, URL::to($post->slug), $post->created_at, $post->slug, $post->content);
+           }
+
+        }
+
+        // first param is the feed format
+        // optional: second param is cache duration (value of 0 turns off caching)
+        // optional: you can set custom cache key with 3rd param as string
+        return $feed->render('atom');
+
+        // to return your feed as a string set second param to -1
+        // $xml = $feed->render('atom', -1);
     }
 }
