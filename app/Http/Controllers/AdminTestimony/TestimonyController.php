ee78259f2732770135bb885067df88401ecbdefd
41Studio <fauziiirachman@gmail.com>
2017-08-22 09:26:22 +0700

    fix testimony error

--- app/Http/Controllers/AdminTestimony/TestimonyController.php
+++ app/Http/Controllers/AdminTestimony/TestimonyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\AdminTestimony;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Cloudder;
 use App\Testimony;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
