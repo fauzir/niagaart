d248bcc6cc2789e3eeb0ae51a598fb66f2313161
41Studio <fauziiirachman@gmail.com>
2017-09-08 11:25:46 +0700

    some fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,8 @@ class Kernel extends ConsoleKernel
             $min = date_format($date, 'i');
             $day = date_format($date, 'j');
             $month = date_format($date, 'n');
-            $schedule->call('App\Http\Controllers\AdminBlog\BlogController@publishArticle', ['id' => $article->id])->cron($min.' '.$hour.' '.$day.' '.$month.' *')->timezone('Asia/Jakarta');
+            // $schedule->call('App\Http\Controllers\AdminBlog\BlogController@publishArticle', ['id' => $article->id])->cron($min.' '.$hour.' '.$day.' '.$month.' *')->timezone('Asia/Jakarta');
+            $schedule->call('App\Http\Controllers\AdminBlog\BlogController@publishArticle', ['id' => $article->id])->dailyAt('11:27');
             $schedule->command('queue:work --once')->cron($min.' '.$hour.' '.$day.' '.$month.' *')->timezone('Asia/Jakarta');
         }
     }
