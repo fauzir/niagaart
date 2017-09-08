a8db85763b2fa4bfd48b3f8b229af9dddc6abbdf
41Studio <fauziiirachman@gmail.com>
2017-09-08 10:27:02 +0700

    some fixes

--- app/Jobs/PublishArticle.php
+++ app/Jobs/PublishArticle.php
@@ -35,7 +35,7 @@ class PublishArticle implements ShouldQueue
         $blog = Blog::find($this->id);
         $blog->published = true;
         $blog->published_at = '';
-        $blog->updated_at = date_format(Carbon::now(), "Y-m-d H:i:s.000");
+        // $blog->updated_at = date_format(Carbon::now(), "Y-m-d H:i:s.000");
         $blog->save();
     }
 }
