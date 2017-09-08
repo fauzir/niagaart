ed561eb5f95b30585025422aac456f9fc47c4115
41Studio <fauziiirachman@gmail.com>
2017-09-08 11:01:10 +0700

    some fixes

--- app/Jobs/PublishArticle.php
+++ app/Jobs/PublishArticle.php
@@ -32,12 +32,15 @@ class PublishArticle implements ShouldQueue
      */
     public function handle()
     {
-        $blog = Blog::find($this->id);
+        // $blog = Blog::find($this->id);
         // $blog->timestamps = false;
-        $blog->published = true;
-        $blog->published_at = '';
-        $blog->updated_at = date_create(Carbon::now());
+        // $blog->published = true;
+        // $blog->published_at = '';
+        // $blog->updated_at = date_create(Carbon::now());
         // $blog->updated_at = date_format(Carbon::now(), "Y-m-d H:i:s.000");
-        $blog->save();
+        // $blog->save();
+
+        Blog::where('id', $this->id)
+            ->update(['published' => true])
     }
 }
